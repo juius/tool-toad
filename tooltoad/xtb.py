333ee726616a98dc19e46f17cb4030378c4d21ad
juius <jseumer@chem.ku.dk>
2023-09-20 12:23:04 +0000

    fix util import

--- tooltoad/xtb.py
+++ tooltoad/xtb.py
@@ -1,12 +1,12 @@
 import logging
 import os
-import subprocess
 import tempfile
-import warnings
 from pathlib import Path
 from typing import List, Tuple
 
 import numpy as np
+
+from tooltoad.utils import check_executable, stream
 
 _logger = logging.getLogger("xtb")
 
@@ -283,35 +283,6 @@ def read_xtb_results(lines: List[str]):
     results.update(properties)
 
     return results
-
-
-def stream(cmd, cwd=None, shell=True):
-    """Execute command in directory, and stream stdout."""
-    popen = subprocess.Popen(
-        cmd,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        universal_newlines=True,
-        shell=shell,
-        cwd=cwd,
-    )
-    for stdout_line in iter(popen.stdout.readline, ""):
-        yield stdout_line
-
-    # Yield errors
-    stderr = popen.stderr.read()
-    popen.stdout.close()
-    yield stderr
-
-    return
-
-
-def check_executable(executable):
-    """Check if executable is in PATH."""
-    results = stream(f"which {executable}")
-    result = list(results)[0]
-    if result.startswith("which: no"):
-        warnings.warn(f"Executable {executable} not found in PATH")
 
 
 if __name__ == "__main__":
