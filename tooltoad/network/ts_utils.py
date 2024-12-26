18526e5b26e78c7f6ff091170fc8748e541fbbc9
juius <hwg245@alumni.ku.dk>
2024-12-26 19:49:51 +0100

    add ts active bonds and checks

--- tooltoad/network/ts_utils.py
+++ tooltoad/network/ts_utils.py
@@ -1,3 +1,7 @@
+from itertools import combinations
+
+import numpy as np
+
 from tooltoad.orca import orca_calculate
 
 
@@ -8,8 +12,6 @@ def preoptimize(
     for i in interactions:
         constraint_str += f"    {{ B {' '.join([str(idx) for idx in i])} C }} \n"
     constraint_str += "  end\nend"
-
-    print(constraint_str)
 
     PREOPT = {"OPT": None}
     PREOPT.update(orca_options)
@@ -33,6 +35,7 @@ def locate_ts(
     active_atoms = " ".join(
         [str(item) for sublist in interaction_indices for item in sublist]
     )
+
     TS = {"OPTTS": None, "freq": None}
     TS.update(orca_options)
     ts_results = orca_calculate(
@@ -43,6 +46,7 @@ def locate_ts(
     {internal_coord_setup}
     TS_Active_Atoms {{ {active_atoms} }} end
     TS_Active_Atoms_Factor 1.5
+    TS_Mode {{ B {int(interaction_indices[0][0])} {int(interaction_indices[0][1])} }} end
     Recalc_Hess 5
     MaxIter 250
     end
@@ -68,3 +72,32 @@ def run_irc(atoms, coords, interaction_indices, orca_options={"XTB2": None}, **k
         **kwargs,
     )
     return irc_results
+
+
+def get_ts_active_bonds(coords, mode, distance_cutoff=2.75, projection_threshold=0.5):
+    coords = np.asarray(coords)
+    mode = np.asarray(mode)
+    n_atoms = mode.shape[0]
+    mode_flat = mode.reshape(-1, 3)
+    atom_pairs = np.array([list(pair) for pair in combinations(range(n_atoms), 2)])
+    vectors = coords[atom_pairs[:, 1]] - coords[atom_pairs[:, 0]]
+    norms = np.linalg.norm(vectors, axis=1, ord=2)
+    valid_mask = (1e-8 < norms) & (norms < distance_cutoff)
+    unit_vectors = vectors[valid_mask] / norms[valid_mask, np.newaxis]
+    mode_vectors = (
+        mode_flat[atom_pairs[valid_mask, 1]] - mode_flat[atom_pairs[valid_mask, 0]]
+    )
+    # Compute projections using element-wise dot products
+    projections = np.einsum("ij,ij->i", mode_vectors, unit_vectors)
+    significant_indices = np.abs(projections) > projection_threshold
+    return (
+        atom_pairs[valid_mask][significant_indices].tolist(),
+        projections[significant_indices].tolist(),
+    )
+
+
+def check_ts(coords, mode, interaction_ids):
+    pairs, _ = get_ts_active_bonds(coords, mode)
+    significant_pairs = {frozenset(pair) for pair in pairs}
+    overlap = interaction_ids & significant_pairs
+    return len(overlap) > 0, [list(pair) for pair in overlap]
