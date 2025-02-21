b82a844933cb938ee5b0d66e35aa39f1e3dfa68b
juius <hwg245@alumni.ku.dk>
2025-02-21 13:21:48 +0100

    add defaults

--- tooltoad/__init__.py
+++ tooltoad/__init__.py
@@ -1,5 +1,5 @@
-from .chemutils import ac2mol, ac2xyz, xyz2ac
+from .chemutils import ac2mol, ac2xyz, hartree2kcalmol, xyz2ac
 from .orca import orca_calculate
 from .periodictable import PeriodicTable
-from .vis import draw3d
+from .vis import draw3d, oneColumnFig, twoColumnFig
 from .xtb import xtb_calculate
